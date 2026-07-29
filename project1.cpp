34ff95ba4a9b77336c6c9c4817efe68bfedcb562
Gamila Hasan <gamila.hasan17@gmail.com>
2026-07-30 03:09:45 +0400

    implement User class mathods

--- project1.cpp
+++ project1.cpp
@@ -17,52 +17,58 @@ private:
 
 public:
     User() {
-        // TODO: Implement default constructor
+        username = "";
+        password = "";
+        phoneNumber = "";
+        status = "Offline";
+        time_t now = time(0);
+        lastSeen = ctime(&now);
     }
 
     User(string uname, string pwd, string phone) {
-        // TODO: Implement parameterized constructor
+        username = uname;
+        password = pwd;
+        phoneNumber = phone;
+        status = "Offline";
+        time_t now = time(0);
+        lastSeen = ctime(&now);
     }
 
     string getUsername() const {
-        // TODO: Implement getter
-        return "";
+        return username;
     }
 
     string getPhoneNumber() const {
-        // TODO: Implement getter
-        return "";
+        return phoneNumber;
     }
 
     string getStatus() const {
-        // TODO: Implement getter
-        return "";
+        return status;
     }
 
     string getLastSeen() const {
-        // TODO: Implement getter
-        return "";
+        return lastSeen;
     }
 
     void setStatus(string newStatus) {
-        // TODO: Implement setter
+        status = newStatus;
     }
 
     void setPhoneNumber(string phone) {
-        // TODO: Implement setter
+        phoneNumber = phone;
     }
 
     void updateLastSeen() {
-        // TODO: Implement last seen update
+        time_t now = time(0);
+        lastSeen = ctime(&now);
     }
 
     bool checkPassword(string pwd) const {
-        // TODO: Implement password check
-        return false;
+        return password == pwd;
     }
 
     void changePassword(string newPwd) {
-        // TODO: Implement password change
+        password = newPwd;
     }
 };
 
