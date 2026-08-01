10b21f1dad99282b0894d03bd62a1aa840263b8a
marwan-osama <mo989989@gmail.com>
2026-08-01 13:16:28 +0300

    add content of the isloggedin method

--- project1.cpp
+++ project1.cpp
@@ -319,8 +319,7 @@ private:
     }
 
     bool isLoggedIn() const {
-        // TODO: Implement login check
-        return false;
+        return currentUserIndex != -1;
     }
 
     string getCurrentUsername() const {
