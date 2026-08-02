5bfbdea1e52ae52fc35f06d56053fe5d21fdb063
gamilahasan17 <gamila.hasan17@gmail.com>
2026-08-02 20:18:34 +0400

    Revert "Logout modifi_2"

--- project1.cpp
+++ project1.cpp
@@ -478,9 +478,24 @@ public:
         vector<string> groupMembers;
         groupMembers.push_back(getCurrentUsername());
 
-    void logout() {
-        if (!isLoggedIn()) {
-            cout << " no user is current logged in " << endl;
+        string memberUsername;
+        cout << "Enter member usernames (type 'done' to finish):\n";
+        while (true) {
+            cout << "Add member: ";
+            cin >> memberUsername;
+            if (memberUsername == "done") {
+                break;
+            }
+
+            int idx = findUserIndex(memberUsername);
+            if (idx != -1) {
+                groupMembers.push_back(memberUsername);
+                cout << memberUsername << " added to group.\n";
+            }
+            else {
+                cout << "User not found!\n";
+            }
+        }
 
         Chat* newGroup = new GroupChat(groupMembers, groupName, getCurrentUsername());
         chats.push_back(newGroup);
@@ -492,8 +507,6 @@ public:
             cout << "No chats available.\n";
             return;
         }
-     
-        currentUserIndex = -1;
 
         cout << "\n--- YOUR CHATS ---\n";
         for (size_t i = 0; i < chats.size(); ++i) {
